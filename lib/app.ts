e4ef7fe0f2f56ad8da6fb17757a5d3d5f8431220
Angel Chavez <angelgce.chavez@gmail.com>
2024-04-04 18:44:55 -0600

    repository

--- lib/app.ts
+++ lib/app.ts
@@ -7,9 +7,9 @@ export class MyPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
+      pipelineName: 'OPA-Pipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('angelgce/OPA-Pipeline', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
