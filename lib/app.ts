918fbb2bb8b8692fcf57b10af89bb889b1c68014
Angel Chavez <angelgce.chavez@gmail.com>
2024-04-06 15:22:14 -0600

    testing first steps for pipelines

--- lib/app.ts
+++ lib/app.ts
@@ -1,11 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stacks/BetaStage';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //pipeline
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'OPA-Pipeline',
       synth: new ShellStep('Synth', {
@@ -13,5 +16,12 @@ export class MyPipelineStack extends cdk.Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+    
+    //beta - stage
+    const betaStage = pipeline.addStage(new MyPipelineAppStage(this, "Beta", {
+      env: { account: "571793088347", region: "us-east-1" }
+    }));
+
+    betaStage.addPost(new ManualApprovalStep('approval'));
   }
 }
---
+++ lib/functions/LambdaTest.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+
+export class MyLambdaStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+
+      new Function(this, 'LambdaTest', {
+        runtime: Runtime.JAVA_17,
+        handler: 'org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest',
+        code: new InlineCode('exports.handler = _ => "Hello, CDK";')
+      });
+    }
+}
