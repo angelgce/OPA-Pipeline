c919ece1d84dd9d2553ed4a3303f523aff46470c
Angel Chavez <angelgce.chavez@gmail.com>
2024-04-06 18:33:48 -0600

    testing first steps for pipelines

--- lib/functions/LambdaTest.ts
+++ lib/functions/LambdaTest.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Code, Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class MyLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +9,7 @@ export class MyLambdaStack extends cdk.Stack {
     new Function(this, 'LambdaFunction', {
       runtime: Runtime.JAVA_17,
       handler: 'org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest',
-      code: new InlineCode('')
+      code: Code.fromAsset('lambda')
     });
   }
 }
